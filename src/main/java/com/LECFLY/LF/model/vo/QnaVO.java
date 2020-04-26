89357a441219e6848660d68d42e4d8fe4c30b72a
Ellena0304 <58832163+Ellena0304@users.noreply.github.com>
2020-04-26 18:29:14 +0900

    private 수정

--- src/main/java/com/LECFLY/LF/model/vo/QnaVO.java
+++ src/main/java/com/LECFLY/LF/model/vo/QnaVO.java
@@ -10,7 +10,7 @@ public class QnaVO {
 	String title;                 //                        제목
 	String content;               //                        내용
 	String file;                  //                        첨부파일 
-	int private;             	//  0                     	공개 여부         (다수인 경우 ,등 구분용 문자 삽입)
+	int showPrivate;             	//  0                     	공개 여부         (다수인 경우 ,등 구분용 문자 삽입)
 	Timestamp writedDay;     	 //  CURRENT_TIMESTAMP     작성날짜          0: 공개 1: 비공개
 	Timestamp updatedDay;    	 //  CURRENT_TIMESTAMP     갱신날짜
 	int hits;          			 //  0                     	조회 수
