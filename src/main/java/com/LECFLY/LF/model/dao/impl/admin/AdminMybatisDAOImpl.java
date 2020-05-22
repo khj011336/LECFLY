9d577e05f2ba05326403548c3db52012a66ffa02
tn-hub <rantatn8@gmail.com>
2020-05-22 14:38:37 +0900

    어드민 통계 구현중

--- src/main/java/com/LECFLY/LF/model/dao/impl/admin/AdminMybatisDAOImpl.java
+++ src/main/java/com/LECFLY/LF/model/dao/impl/admin/AdminMybatisDAOImpl.java
@@ -1,12 +1,12 @@
-package com.LECFLY.LF.service.impl.admin;
+package com.LECFLY.LF.model.dao.impl.admin;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
 import com.LECFLY.LF.model.dao.inf.admin.IAdminLectureDAO;
 import com.LECFLY.LF.model.vo.cart.CouponVO;
@@ -14,14 +14,12 @@ import com.LECFLY.LF.model.vo.creator.KitVO;
 import com.LECFLY.LF.model.vo.creator.LectureVO;
 import com.LECFLY.LF.model.vo.creator.VideoVO;
 import com.LECFLY.LF.model.vo.virtual.CategoryLectureStatVO;
-import com.LECFLY.LF.service.inf.admin.IAdminLectureSVC;
 
-@Service
-public class AdminLectureSVCImpl implements IAdminLectureSVC {
+@Repository("mybatisAdmin")
+public class AdminMybatisDAOImpl implements IAdminLectureDAO{
 
 	@Autowired
-	@Qualifier("mybatisAdmin")
-	private IAdminLectureDAO adDAO;
+	private SqlSessionTemplate sstem;
 	
 	@Override
 	public boolean insertLecture(LectureVO vo) {
@@ -49,7 +47,6 @@ public class AdminLectureSVCImpl implements IAdminLectureSVC {
 
 	@Override
 	public List<LectureVO> selectLectureList() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -144,108 +141,87 @@ public class AdminLectureSVCImpl implements IAdminLectureSVC {
 	}
 
 	@Override
-	public Map<String,Integer> checkLectureMaxPageNumber() {
-		int totalRecords = adDAO.checkNumberOfLectures();
-		int maxPg = totalRecords / AD_PAGE_SIZE + (
-				totalRecords % AD_PAGE_SIZE == 0 ? 0 : 1 );
-		HashMap<String,Integer> rMap = new HashMap<>();
-		rMap.put("totalRecords", totalRecords);
-		rMap.put("maxPg", maxPg);
-		return rMap;
+	public int checkNumberOfLectures() {
+		return sstem.selectOne("IAdminDAO.SQL_SELECT_LECTURE_NUMBER");
 	}
 
 	@Override
-	public List<LectureVO> selectAllLecture(int pageNumber) {
-		int offset = (pageNumber -1)*AD_PAGE_SIZE;
-		List<LectureVO> lecList = adDAO.searchLectureForAll(offset, AD_PAGE_SIZE);
-		return lecList;
+	public List<LectureVO> searchLectureForAll(int offset, int limit) {
+		Map<String,Object> map = new HashMap<>();
+		map.put("offset", offset);
+		map.put("limit", limit);
+		return sstem.selectList("IAdminDAO.SQL_SELECT_LECTURE_ALL_PG",map);
 	}
 
 	@Override
-	public List<VideoVO> selectAllVideo(int pageNumber) {
-		int offset = (pageNumber -1)*AD_PAGE_SIZE;
-		List<VideoVO> vdList = adDAO.searchVideoForAll(offset, AD_PAGE_SIZE);
-		return vdList;
+	public List<VideoVO> searchVideoForAll(int offset, int limit) {
+		Map<String,Object> map = new HashMap<>();
+		map.put("offset", offset);
+		map.put("limit", limit);
+		return sstem.selectList("IAdminDAO.SQL_SELECT_VIDEO_ALL_PG",map);
 	}
 
 	@Override
-	public Map<String, Integer> checkVideoMaxPageNumber() {
-		int totalRecords = adDAO.checkNumberOfVideos();
-		int maxPg = totalRecords / AD_PAGE_SIZE + (
-				totalRecords % AD_PAGE_SIZE == 0 ? 0 : 1 );
-		HashMap<String,Integer> rMap = new HashMap<>();
-		rMap.put("totalRecords", totalRecords);
-		rMap.put("maxPg", maxPg);
-		return rMap;
+	public int checkNumberOfVideos() {
+		return sstem.selectOne("IAdminDAO.SQL_SELECT_VIDEO_NUMBER");
+	}
+
+	@Override
+	public int checkNumberOfLecturesSearchFilter(Map<String, Object> condition) {
+		return sstem.selectOne("IAdminDAO.SQL_SELECT_LECTURE_NUMBER_BY_FILTER", condition);
 	}
 
 	@Override
 	public List<LectureVO> selectLectureListSearchFilter(Map<String, Object> condition) {
-		String pn = (String) condition.get("pn");
-		int pageNumber = Integer.parseInt(pn);
-		int offset =(pageNumber-1)*AD_PAGE_SIZE;
-		condition.put("offset", offset);
-		condition.put("limit", AD_PAGE_SIZE);
-		System.out.println(condition);
-		List<LectureVO> list = adDAO.selectLectureListSearchFilter(condition);
-		System.out.println("결과수 : "+ list.size());
-		return adDAO.selectLectureListSearchFilter(condition);
+		return sstem.selectList("IAdminDAO.SQL_SELECT_LECTURE_BY_FILTER_PG", condition);
 	}
-
-	@Override
-	public Map<String, Integer> checkLectureMaxPageNumberSearchFilter(Map<String, Object> condition) {
-		int totalRecords = adDAO.checkNumberOfLecturesSearchFilter(condition);
-		int maxPg = totalRecords / AD_PAGE_SIZE + (
-				totalRecords % AD_PAGE_SIZE == 0 ? 0 : 1 );
-		HashMap<String,Integer> rMap = new HashMap<>();
-		rMap.put("totalRecords", totalRecords);
-		rMap.put("maxPg", maxPg);
-		return rMap;
-	}
-
-	@Override
-	public int selectLectureApproval() {
-		return adDAO.selectLectureApproval();
-	}
-
-	@Override
-	public int selectLectureAll() {
-		return adDAO.selectLectureAll();
-	}
-
+	
+	
+	// 관리자 메인 통계
+	// 방문자수
 	@Override
 	public int selectTodayInCnt() {
-		return adDAO.selectTodayInCnt();
+		return sstem.selectOne("IAdminDAO.SQL_SELECT_TODAY_IN_CNT");
 	}
-
+	// 회원수
 	@Override
 	public int selectMemberCnt() {
-		return adDAO.selectMemberCnt();
+		return sstem.selectOne("IAdminDAO.SQL_SELECT_MEMBER_CNT");
 	}
-
+	// 업로더수
 	@Override
 	public int selectCreatorCnt() {
-		return adDAO.selectCreatorCnt();
+		return sstem.selectOne("IAdminDAO.SQL_SELECT_CREATOR_CNT");
 	}
-
+	// 전체 강의수
+	@Override
+	public int selectLectureAll() {
+		return sstem.selectOne("IAdminDAO.SQL_SELECT_LECTURE_CNT_ALL");
+	}
+	// 신규회원수
 	@Override
 	public int selectNewMemberCnt() {
-		return adDAO.selectNewMemberCnt();
+		return sstem.selectOne("IAdminDAO.SQL_SELECT_NEW_MEMBER_CNT");
 	}
-
+	// 업로더 승인대기수 
 	@Override
 	public int selectCreatorApprovalCnt() {
-		return adDAO.selectCreatorApprovalCnt();
+		return sstem.selectOne("IAdminDAO.SQL_SELECT_CREATOR_APPROVAL_CNT");
 	}
-
+	// 강의 승인대기수	
+	@Override
+	public int selectLectureApproval() {
+		return sstem.selectOne("IAdminDAO.SQL_SELECT_LECTURE_APPROVAL_CNT");
+	}
+	// 문의 답변하기 수	
 	@Override
 	public int selectQnaCommentCnt() {
-		return adDAO.selectQnaCommentCnt();
+		return sstem.selectOne("IAdminDAO.SQL_SELECT_QNA_COMMENT_CNT");
 	}
-
+	
+	// 카테고리별 강의수
 	@Override
 	public List<CategoryLectureStatVO> selectCategoryLectureCnt() {
-		return adDAO.selectCategoryLectureCnt();
+		return sstem.selectList("IAdminDAO.SQL_SELECT_CATEGORY_LECTURE");
 	}
-
 }
